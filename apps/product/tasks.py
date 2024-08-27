5e1a20f99cebb82bcecb759f309d639b3e24f365
Shana Malek <shanamalek157@gmail.com>
2024-08-28 00:24:16 +0530

    [FIX] num_of_product generate

--- apps/product/tasks.py
+++ apps/product/tasks.py
@@ -16,21 +16,22 @@ def generate_dummy_products_data(no_of_products, user_id):
     print("<>>>>>>>>>>>>>>>>>>>>>>", no_of_products, user_id)
     user = User.objects.get(id=user_id)
     start = time.time()
-    for _ in range(10):
-        name = ''.join(random.choices(string.ascii_uppercase, k=5))
-        category = Category.objects.create(name=name, created_by = user)
-        for _ in range(100):
-            prod_name = ''.join(random.choices(string.ascii_uppercase, k=5))
-            price = random.randrange(1.00, 10000.00)
-            product = Product.objects.create(
-                category_id = category.id,
-                title = prod_name,
-                description = f'This Product belongs to Category-{category.name}',
-                status = 'draft',
-                created_by = user,
-                price = price
-            )
-        duration = (time.time() - start) * 1000
+    # for _ in range(10):
+    name = ''.join(random.choices(string.ascii_uppercase, k=5))
+    category = Category.objects.create(name=name, created_by = user)
+    print('category: ', category)
+    for _ in range(no_of_products):
+        prod_name = ''.join(random.choices(string.ascii_uppercase, k=5))
+        price = random.randrange(1.00, 10000.00)
+        product = Product.objects.create(
+            category_id = category.id,
+            title = prod_name,
+            description = f'This Product belongs to Category-{category.name}',
+            status = 'draft',
+            created_by = user,
+            price = price
+        )
+    duration = (time.time() - start) * 1000
 
 
 # @shared_task
